f260c58da542b14d173cc2d642a03b72f636cc9c
JonathanRodriguezC <89150664+JonathanRodriguezC@users.noreply.github.com>
2023-11-16 09:11:30 -0600

    actua

--- CRUD-LocalStorage/script/script.js
+++ CRUD-LocalStorage/script/script.js
@@ -1,5 +1,79 @@
+function validateData(){
+    let name=document.getElementById("name").value.trim();
+    let price=document.getElementById("price").value.trim();
+    let description=document.getElementById("description").value.trim();
+    let image=document.getElementById("inputGroupFile01");
+    if(name==""){
+        document.getElementById("name-error-msg").innerHTML="You must enter the name"
+        return false;
+    }
+    else{
+        document.getElementById("name-error-msg").innerHTML="";
+        
+    }
+    if(price==""){
+        document.getElementById("price-error-msg").innerHTML="You must enter the price";
+        return false
+    }
+    else if(parseFloat(price)<=0|| isNaN(parseFloat(price))){
+        document.getElementById("price-error-msg").innerHTML="You Must enter a valid price";
+        return false
+    }
+    else{
+        document.getElementById("price-error-msg").innerHTML="";
+    }
+     if(description==""){
+        document.getElementById("disc-error-msg").innerHTML="You must enter the descripcion";
+        return false
+    }
+    else if(description.length>100){
+        document.getElementById("disc-error-msg").innerHTML="descripcion max legth is 100 caracters";
+        return false
+
+    }
+    else{
+        document.getElementById("disc-error-msg").innerHTML="";
+    }
+
+    if(image.files.length==0){
+        document.getElementById("image-error-msg").innerHTML="You must select a image";
+        return false
+    }
+    else{
+         document.getElementById("image-error-msg").innerHTML="";
+        
+    }
+    let allowedFormats=/(\.jpg|\.jpep|\.png|\.web)$/i;
+    
+    if(!allowedFormats.exec(image.files[0].name)){
+        document.getElementById("image-error-msg").innerHTML="you must select a value image";
+        image.value="";
+        return false;
+    }
+    
+    else{
+        document.getElementById("image-error-msg").innerHTML="";
+    }
+    let filesSize=image.files[0].size/1024;
+    if(filesSize>700){
+        document.getElementById("image-error-msg").innerHTML="you max file size is 700";
+        image.value="";
+        return false;
+    }
+    else{
+        document.getElementById("image-error-msg").innerHTML="";
+    }
+    
+
+
+        return true;
+}
+
+
 function AddData(){
-    let name=document.getElementById("name").value;
+    
+    if(validateData()){
+        let name=document.getElementById("name").value;
     let price=document.getElementById("price").value;
     let description=document.getElementById("description").value;
     let image=document.getElementById("inputGroupFile01");
@@ -34,8 +108,13 @@ function AddData(){
     document.getElementById("inputGroupFile01").value="";
     document.getElementById("btn-close").click();
     alert("product added sucefully");
+    location.reload();
     showData();
+    }
 }
+
+
+
 function showData(){
     let productList;
     if(localStorage.getItem("productList")==null){
@@ -62,11 +141,43 @@ function showData(){
                                 <div class="card-body" style=height:11 rem; width:16rem">
                                 <h5 class="card-title text-center"> Item # ${product.id}</h5>
                                 <img src="${product.image}" class="card-img-top">
-                                </div></div></div></div></div></div>`;
+                                </div>
+                                <ul class="list-group">
+                                    <li class="list-group-item"><strong>Name:</strong>${product.name}</li>
+                                    <li class="list-group-item"><strong>Price:</strong>${product.price}</li>
+                                    <li class="list-group-item"><strong>Decription:</strong>${product.description}</li>
+                                </ul>
+                                <div class="card-body text-center">
+                                <button class="btn btn-success">Edit</button>
+                                <button onclick='deleteProduct(${product.id})'  class="btn btn-danger">Delete</button>
+
+                                </div>
+                                </div></div></div></div></div>`;
                 ;
             }
         }
         document.getElementById("crud-table").innerHTML=html;
     
+}
+function deleteProduct(id){
+    alert(id);
+    let index=0;
+    let indexDelete;
+    let productList=JSON.parse(localStorage.getItem("productList"))
+    /*for(const element of productList){
+        if(id==element.id){
+            console.log("found")
+            indexDelete=index;
+            break;
+        }
+        index++;
+
+    }*/
+
+    productList=productList.filter(product=>product.id!=id);
+    localStorage.setItem("productList",JSON.stringify(productList));
+    
+    location.reload();
+
 }
 showData();
